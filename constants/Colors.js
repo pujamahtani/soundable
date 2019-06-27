e2af1612db279d6eea16e82745c87df43dee2864
puja <poomahtani@gmail.com>
2019-06-27 10:57:23 +0530

    First Commit

---
+++ constants/Colors.js
@@ -0,0 +1,25 @@
+const tintColor = '#2f95dc';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+
+//Our Colors
+
+primaryColor: '#191B2F',
+  accentColor:'#9458EA',
+  placeholderColor:'#989CC1',
+  accentGradientStart:'#8250AF',
+  accentGradientEnd:'#3A2496',
+  primaryGradientStart:'#1E2542',
+  primaryGradientEnd:'#16162C',
+  headingColor:'#fff',
+};
